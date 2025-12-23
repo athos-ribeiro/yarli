9d48cefd74df5bb1b147efd8fcec7faf479c1aa9
Athos Ribeiro <athos@ar.dev.br>
2025-12-23 08:59:21 -0300

    Drop visitor pattern in favor of pattern matching
    The visitor pattern is used in the book to  workaround a OO paradigm
    limitation, as described in the final part of Chapter 5. Since rust
    provides pattern matching, using the visitor pattern here would only
    bloat our codebase for now. Let's implement it using pattern matching,
    as suggested by challenge 2 in Ch. 5.

--- src/parser.rs
+++ src/parser.rs
@@ -8,24 +8,6 @@ pub enum Expr<'a> {
     Unary { operator: Token, right: &'a Expr<'a> }
 }
 
-impl<'a> Expr<'a> {
-    fn accept<T>(&self, visitor: &dyn Visitor<T>) -> T {
-        match self {
-            Self::Binary { .. } => visitor.visit_binary_expr(self),
-            Self::Grouping { .. } => visitor.visit_grouping_expr(self),
-            Self::Literal { .. } => visitor.visit_literal_expr(self),
-            Self::Unary { .. }=> visitor.visit_unary_expr(self)
-        }
-    }
-}
-
-pub trait Visitor<T> {
-    fn visit_binary_expr(&self, expr: &Expr) -> T;
-    fn visit_grouping_expr(&self, expr: &Expr) -> T;
-    fn visit_literal_expr(&self, expr: &Expr) -> T;
-    fn visit_unary_expr(&self, expr: &Expr) -> T;
-}
-
 pub struct AstPrinter;
 
 impl AstPrinter {
@@ -33,50 +15,26 @@ impl AstPrinter {
         let mut my_str = format!("({name}");
         for expr in exprs {
             my_str.push_str(" ");
-            my_str.push_str(&expr.accept(self));
+            my_str.push_str(&self.print(&expr));
         }
         my_str.push_str(")");
         my_str
     }
 
     pub fn print(&self, expr: &Expr) -> String {
-        expr.accept(self)
-    }
-}
-
-impl Visitor<String> for AstPrinter {
-    fn visit_binary_expr(&self, expr: &Expr) -> String {
-        if let Expr::Binary { left, operator, right } = expr {
-            self.parenthesize(&operator.lexeme, vec![left, right])
-        } else {
-            panic!()
-        }
-    }
-
-    fn visit_grouping_expr(&self, expr: &Expr) -> String {
-        if let Expr::Grouping { expression } = expr {
-            self.parenthesize(&String::from("group"), vec![expression])
-        } else {
-            panic!()
-        }
-    }
-
-    fn visit_literal_expr(&self, expr: &Expr) -> String {
-        if let Expr::Literal { value } = expr {
-            if value.is_none() {
-                return String::from("nil")
+        match expr {
+            Expr::Binary { left, operator, right } =>
+                self.parenthesize(&operator.lexeme, vec![left, right]),
+            Expr::Grouping { expression } =>
+                self.parenthesize(&String::from("group"), vec![expression]),
+            Expr::Literal { value } => {
+                if value.is_none() {
+                    return String::from("nil")
+                }
+                value.as_ref().unwrap().to_string()
             }
-            value.as_ref().unwrap().to_string()
-        } else {
-            panic!()
-        }
-    }
-
-    fn visit_unary_expr(&self, expr: &Expr) -> String {
-        if let Expr::Unary { operator, right } = expr {
-            self.parenthesize(&operator.lexeme, vec![right])
-        } else {
-            panic!()
+            Expr::Unary { operator, right } =>
+                self.parenthesize(&operator.lexeme, vec![right]),
         }
     }
 }
