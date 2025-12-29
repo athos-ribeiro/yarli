84012db6cdda77e2f2caad8c54105e379ee016b4
Athos Ribeiro <athos@ar.dev.br>
2025-12-29 16:52:36 -0300

    Adjust tests for new Expr types

--- tests/test_ast_printer.rs
+++ tests/test_ast_printer.rs
@@ -4,13 +4,15 @@ use yarli::parser::{Expr, AstPrinter};
 #[test]
 fn ast_printer_test() {
     let expected = "(* (- 123) (group 45.67))";
+    let star_token = Token {token_type: TokenType::STAR, lexeme: String::from("*"), literal: None, line: 1 };
+    let minus_token = Token {token_type: TokenType::MINUS, lexeme: String::from("-"), literal: None, line: 1 };
     let expression = Expr::Binary {
-        left: &Expr::Unary {
-            operator: Token {token_type: TokenType::MINUS, lexeme: String::from("-"), literal: None, line: 1 },
-            right: &Expr::Literal { value: Some(Box::new(123)) }
-        },
-        operator: Token {token_type: TokenType::STAR, lexeme: String::from("*"), literal: None, line: 1 },
-        right: &Expr::Grouping { expression: &Expr::Literal {value: Some(Box::new(45.67))} }
+        left: Box::new(Expr::Unary {
+            operator: &minus_token,
+            right: Box::new(Expr::Literal { value: Some(Box::new(123)) })
+        }),
+        operator: &star_token,
+        right: Box::new(Expr::Grouping { expression: Box::new(Expr::Literal {value: Some(Box::new(45.67))}) })
     };
 
     let result = AstPrinter.print(&expression);
--- tests/test_rpn_printer.rs
+++ tests/test_rpn_printer.rs
@@ -4,22 +4,25 @@ use yarli::parser::{Expr, RpnPrinter};
 #[test]
 fn rpn_printer_test() {
     let expected = "1 2 + 4 3 - *";
+    let plus_token = Token {token_type: TokenType::PLUS, lexeme: String::from("+"), literal: None, line: 1 };
+    let star_token = Token {token_type: TokenType::STAR, lexeme: String::from("*"), literal: None, line: 1 };
+    let minus_token = Token {token_type: TokenType::MINUS, lexeme: String::from("-"), literal: None, line: 1 };
     let expression = Expr::Binary {
-        left: &Expr::Grouping {
-            expression: &Expr::Binary {
-                left: &Expr::Literal {value: Some(Box::new(1))},
-                operator: Token {token_type: TokenType::PLUS, lexeme: String::from("+"), literal: None, line: 1 },
-                right: &Expr::Literal {value: Some(Box::new(2))}
-            }
-        },
-        operator: Token {token_type: TokenType::STAR, lexeme: String::from("*"), literal: None, line: 1 },
-        right: &Expr::Grouping {
-            expression: &Expr::Binary {
-                left: &Expr::Literal {value: Some(Box::new(4))},
-                operator: Token {token_type: TokenType::MINUS, lexeme: String::from("-"), literal: None, line: 1 },
-                right: &Expr::Literal {value: Some(Box::new(3))}
-            }
-        }
+        left: Box::new(Expr::Grouping {
+            expression: Box::new(Expr::Binary {
+                left: Box::new(Expr::Literal {value: Some(Box::new(1))}),
+                operator: &plus_token,
+                right: Box::new(Expr::Literal {value: Some(Box::new(2))})
+            })
+        }),
+        operator: &star_token,
+        right: Box::new(Expr::Grouping {
+            expression: Box::new(Expr::Binary {
+                left: Box::new(Expr::Literal {value: Some(Box::new(4))}),
+                operator: &minus_token,
+                right: Box::new(Expr::Literal {value: Some(Box::new(3))})
+            })
+        })
     };
 
     let result = RpnPrinter.print(&expression);
